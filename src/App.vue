1878a24283fd4fdc0938949aa45449c1997c651e
Johan Melin <soldierheart@hotmail.com>
2022-01-12 14:54:38 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,135 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <Header 
+    @toggle-add-task="toggleAddTask" 
+    title="Task Tracker" 
+    :showAddTask="showAddTask"
+  />
+  <div v-show="showAddTask">
+    <AddTask @add-task="addTask" />
+  </div>
+  <Tasks 
+    @delete-task="deleteTask" 
+    @toggle-reminder="toggleReminder"
+    :tasks="tasks" 
+  />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks,
+    AddTask
+  },
+  data(){
+    return {
+      tasks: [],
+      showAddTask: false
+    }
+  },
+  methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task){
+      this.tasks = [...this.tasks, task];
+    },
+    deleteTask(id){
+      if(confirm("Are you sure you want to delete this task?")){
+        this.tasks = this.tasks.filter(task => task.id !== id)
+      }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map(task => 
+      task.id === id ? {...task, reminder: !task.reminder} 
+      : task
+    )},
+    async fetchTasks(){
+      const res = await fetch('api/tasks');
+      const data = await res.json();
+      return data;
+    },
+    async fetchTask(id){
+      const res = await fetch(`api/tasks/${id}`);	
+      const data = await res.json();
+      return data;
+    }
+  },
+  beforeCreate(){
+    console.log(" beforeCreate")
+  },
+  async created(){
+    console.log("created")
+    this.tasks = await this.fetchTasks();
+  },
+  beforeMount(){
+    console.log("beforeMount")
+  }, 
+  mounted(){
+    console.log("mounted")
+  }, 
+  beforeUpdate(){
+    console.log("beforeUpdate")
+  }, 
+  updated(){
+    console.log("updated")
+  }, 
+  beforeUnmount(){
+    console.log("beforeUnmount")
+  }, 
+  unmounted(){
+    console.log("unmounted")
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
